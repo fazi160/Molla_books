501cae2e22029f4a80ea64bc621ec37ac4437083
fazil <fazifazil160160@gmail.com>
2024-02-08 16:51:06 +0530

    change

--- ecom/settings.py
+++ ecom/settings.py
@@ -103,23 +103,17 @@ IMAGEKIT_DEFAULT_CACHEFILE_STRATEGY = 'imagekit.cachefiles.strategies.Optimistic
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.postgresql',
-#        'NAME': config('dbbase'),
-#        'USER':config('dbuser'),
-#        'PASSWORD': config('dbpass'),
-#        'HOST': 'localhost',
-#        'PORT': config('dbport'),
-#    }
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': config('dbbase'),
+       'USER':config('dbuser'),
+       'PASSWORD': config('dbpass'),
+       'HOST': 'localhost',
+       'PORT': config('dbport'),
+   }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
